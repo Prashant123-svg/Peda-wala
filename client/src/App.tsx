6c7bc1b2f2351b516421d365162fb4099f559783
Prashant123-svg <prashantchraya@gmail.com>
2026-05-05 08:48:51 +0530

    feat: add Google OAuth to signup page and improve checkout UX

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -74,10 +74,7 @@ function AppContent() {
             <Route path="/category/:name" element={<CategoryDetail />} />
             <Route path="/categories" element={<Categories />} />
             <Route path="/products" element={<Products />} />
-            <Route
-              path="/orders"
-              element={token ? <Orders /> : <Navigate to="/login" replace state={{ from: location.pathname }} />}
-            />
+            <Route path="/orders" element={<Orders />} />
             <Route
               path="/orders/:orderId"
               element={token ? <OrderDetails /> : <Navigate to="/login" replace state={{ from: location.pathname }} />}
